8978b37eb7d9260029ca848b56f8724ba4e548c1
santhiya2 <61583776+santhiya2@users.noreply.github.com>
2023-02-16 23:22:54 -0600

    script update

---
+++ scripts/backup.sh
@@ -0,0 +1,13 @@
+
+!/bin/bash
+
+#creating backup 
+
+echo "creat backup directory"
+ mkdir /home/santhiya_regnex/resources_backup >> echo "directory exists"
+
+cp -r  /home/santhiya_regnex/resources/*   /home/santhiya_regnex/resources_backup
+
+echo "file copied"
+
+
